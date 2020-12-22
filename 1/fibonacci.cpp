5a98142a986e86f9fb8553805c93302e3390e000
rohit-0308 <rohitkumar860311@gmail.com>
2020-12-22 08:30:49 +0530

    Added Linked List folder

--- 1/fibonacci.cpp
+++ 1/fibonacci.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-void fibonacci(int n){
-    long long t1=0;
-    long long t2=1;
+void fibonacci(int n)
+{
+    long long t1 = 0;
+    long long t2 = 1;
     long long nextTerm;
-    for(int i=1;i<=n;i++){
-        cout<<t1<<"  ";
-        nextTerm=t1+t2;
-        t1=t2;
-        t2=nextTerm;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << t1 << "  ";
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
     }
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     fibonacci(n);
     return 0;
 }
--- 20. Linked Listed/linkedList.cpp
+++ 20. Linked Listed/linkedList.cpp
@@ -50,6 +50,65 @@ bool search(node *head, int key)
     return false;
 }
 
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+
+    delete todelete;
+}
+
+void deletenode(node *&head, int val)
+{
+    if (head == NULL)
+        return;
+
+    if (head->next == NULL)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
+node *reverse(node *&head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+
+    return prevptr;
+}
+
+node *reverseRecursive(node *&head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    node *newhead = reverseRecursive(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return newhead;
+}
+
 void display(node *head)
 {
     node *temp = head;
@@ -64,12 +123,26 @@ void display(node *head)
 int main()
 {
     node *head = NULL;
+    // insertAtTail(head, 1);
+    // insertAtTail(head, 2);
+    // insertAtTail(head, 3);
+    // display(head);
+    // insertAtHead(head, 4);
+    // display(head);
+    // cout << search(head, 5);
+    // deletenode(head, 2);
+    // deleteAtHead(head);
+    // display(head);
+
+    cout << endl;
+    cout << endl;
+
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
+    insertAtTail(head, 4);
     display(head);
-    insertAtHead(head, 4);
-    display(head);
-    cout << search(head, 5);
+    node *newhead = reverseRecursive(head);
+    display(newhead);
     return 0;
 }
