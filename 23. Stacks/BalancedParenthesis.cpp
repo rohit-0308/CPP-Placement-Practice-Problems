90baba201eb9056e84ca43fd5518e36ddb8dc97b
rohit-0308 <rohitkumar860311@gmail.com>
2021-06-25 20:14:16 +0530

    Balanced Parenthesis Question

---
+++ 23. Stacks/BalancedParenthesis.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stack>
+#include <string.h>
+
+using namespace std;
+
+bool isBalanced(string s)
+{
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            st.push(s[i]);
+        else if (s[i] == ')')
+        {
+            if (!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        else if (s[i] == '}')
+        {
+            if (!st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        else if (s[i] == ']')
+        {
+            if (!st.empty() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
+    string s = "({[](){}})";
+
+    cout << isBalanced(s);
+
+    return 0;
+}
